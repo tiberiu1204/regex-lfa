6d428e4910c940aeec0b1433f93046bbb847415d
Tiberiu Popescu <tiberiu1204@gmail.com>
2024-04-08 18:53:50 +0300

    Added light testing in main

--- main.cpp
+++ main.cpp
@@ -1,50 +1,19 @@
 #include "lambda_nfa.h"
-#include <fstream>
 #include "regex_engine.h"
+#include <regex>
 
 int main() {
-//    std::ifstream in("input.txt");
-//    std::ofstream out("output.txt");
-//    if(!in.is_open()) {
-//        std::cout<<"Input file could not be opened.\n";
-//        return -1;
-//    }
-//    Automata automata;
-//    int num_states;
-//    in >> num_states;
-//    for(int i = 0; i < num_states; i++) {
-//        int state;
-//        in >> state;
-//        automata.insert_node(state);
-//    }
-//    int num_trans;
-//    in >> num_trans;
-//    for(int i = 0; i < num_trans; i++) {
-//        int dest_state, src_state;
-//        char trans_char;
-//        in >> src_state >> dest_state >> trans_char;
-//        automata.insert_edge(dest_state, src_state, trans_char);
-//    }
-//    int initial_state;
-//    in >> initial_state;
-//    automata.set_init_node(initial_state);
-//    int num_term_nodes;
-//    in >> num_term_nodes;
-//    for(int i = 0; i < num_term_nodes; i++) {
-//        int state;
-//        in >> state;
-//        automata.add_term_node(state);
-//    }
-//    int num_words;
-//    in >> num_words;
-//    for(int i = 0; i < num_words; i++) {
-//        std::string word;
-//        in >> word;
-//        if(automata.accept(word)) std::cout<<"DA\n";
-//        else std::cout<<"NU\n";
-//    }
-//    in.close();
-//    out.close();
-    Regex reg("ab(cd|ef)*");
-    std::cout<<reg.eval("abcdcdefcdefcdefab");
+    std::vector<std::string> regex_arr = {"ab(cd|ef)*", "abcdefg", "(abc)*", "abc*", "abcc*", "(ab|c)*", "abc(def(hij)*)*"};
+    std::vector<std::string> match_arr = {"abcdefefcdefef", "abcdefg", "abcabcabc", "ab", "abccc", "abcccababc", "abcdefhijhijdefhijhij"};
+    for(const auto &expr : regex_arr) {
+        Regex reg(expr);
+        std::regex reg_std(expr);
+        for(const auto &match : match_arr) {
+            if(reg.eval(match) != std::regex_match(match, reg_std)) {
+                std::cout<<"Failed at regex '"<<expr<<"' with match word '"<<match<<"'\n";
+                return -1;
+            }
+        }
+    }
+    std::cout<<"All tests passed\n";
 }
